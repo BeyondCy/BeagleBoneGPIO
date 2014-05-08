2b072f8603030e27bfad6d336efa66a4c1c5b302
zerosum0x0 <zerosum0x0@gmail.com>
2014-05-07 23:52:29 -0600

    oscillation example

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "BBGPIO/BBGPIO.h"
+
+// This will send a signal oscillating high/low every 1 second
 
 int main(int argc, char* argv[])
 {
-	PinFactory::reclaimAll();
+	Pin* p8_26 = PinFactory::slot(PinSlot::P8_26, PinDirection::OUTPUT);
+	
+	while (true)
+	{
+		p8_26->isHigh() ? p8_26->setLow() : p8_26->setHigh();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
 
 	return 0;
 }
