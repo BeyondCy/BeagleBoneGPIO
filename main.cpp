6c4de28d8808450994edb2d4f6b8b683eee875a9
zerosum0x0 <zerosum0x0@gmail.com>
2014-05-23 18:56:12 -0600

    added debug output

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <chrono>
 #include <thread>
 
+#include <iostream>
+
 #include "BBGPIO/BBGPIO.h"
 
 // This will send a signal oscillating high/low every 1 second
 
 int main(int argc, char* argv[])
 {
-	Pin* p8_26 = PinFactory::slot(PinSlot::P8_26, PinDirection::OUTPUT);
+	Pin* p9_11 = PinFactory::slot(PinSlot::P9_11, PinDirection::OUTPUT);
 	
 	while (true)
 	{
-		p8_26->isHigh() ? p8_26->setLow() : p8_26->setHigh();
+		p9_11->isHigh() ? p9_11->setLow() : p9_11->setHigh();
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		std::cout << p9_11->isHigh() ? "High" : "Low" << std::endl;
 	}
 
 	return 0;
